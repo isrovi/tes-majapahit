5041a9c64521f43a13f999ea81ebffa629f2d3b9
isrovi <bagus.isrovi@gmail.com>
2021-12-06 17:34:04 +0700

    tes majapahit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Row, Card, Button } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+  const [post, setPost] = useState();
+
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  const getPosts = () => {
+    axios
+      .get(`${url}`)
+      .then((res) => {
+        setPost(res.data);
+        console.log(res);
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-2">
+      <h1>Get All Post</h1>
+      <Row className="p-5 justify-content-between">
+        {post?.map((item, index) => (
+          <Card
+            bg="light"
+            class="mb-4"
+            key={index}
+            item={item}
+            style={{ width: "18rem", marginBottom: 20 }}
+          >
+            <Card.Body>
+              <Card.Title>{item.title}</Card.Title>
+              <Card.Text>{item.body}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </Row>
     </div>
   );
 }
